9e2ae517c0efe031dfc950ec5dc33520bf98b6b9
Zed <zedcaoile@gmail.com>
2025-08-09 10:44:11 +0800

    submit exer 1.7

--- part 1/courseinfo/src/App.jsx
+++ part 1/courseinfo/src/App.jsx
@@ -5,32 +5,42 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const total = good + neutral + bad;
+  const average = total === 0 ? 0 : (good - bad) / total;
+  const positive = total === 0 ? 0 : (good / total) * 100
 
   const handleGoodClick = () => {
     setGood(good + 1);
-    console.log(good)
+    console.log(good);
   };
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1);
-    console.log(good)
+    console.log(good);
   };
-  
+
   const handleBadClick = () => {
     setBad(bad + 1);
-    console.log(good)
+    console.log(good);
   };
 
   return (
     <div>
-      <p><b>give feedback</b></p>
+      <p>
+        <b>give feedback</b>
+      </p>
       <button onClick={handleGoodClick}>good</button>
       <button onClick={handleNeutralClick}>neutral</button>
       <button onClick={handleBadClick}>bad</button>
-      <p><b>statistics</b></p>
+      <p>
+        <b>statistics</b>
+      </p>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {average}</p>
+      <p>positive {positive}%</p>
     </div>
   );
 };
