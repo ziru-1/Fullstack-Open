0ba8a2dd931e874fe9cd437cca62eaea3f1380aa
Zed <zedcaoile@gmail.com>
2025-08-10 17:02:18 +0800

    submit exer 2.1

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,54 @@
+const Header = (props) => <h1>{props.name}</h1>
+
+const Content = (props) => (
+  <div>
+    <Part part={props.parts[0]} />
+    <Part part={props.parts[1]} />
+    <Part part={props.parts[2]} />
+  </div>
+)
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+)
+
+const Total = (props) => <p>Number of exercises {props.total}</p>
+
+const Course = (props) => {
+  return (
+    <>
+      <Header name={props.course.name}/>
+      <Content parts={props.course.parts}/>
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
