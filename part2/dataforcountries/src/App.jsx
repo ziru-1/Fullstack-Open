bfc52e0132094bb096618605a78f14a52706f172
Zed <zedcaoile@gmail.com>
2025-08-24 11:29:21 +0800

    submit exer 2.20

--- part2/dataforcountries/src/App.jsx
+++ part2/dataforcountries/src/App.jsx
@@ -7,19 +7,30 @@ const App = () => {
   const [search, setSearch] = useState("");
   const [filteredCountries, setFilteredCountries] = useState([]);
   const [selectedCountry, setSelectedCountry] = useState(null);
+  const [selectedCountryWeather, setSelectedCountryWeather] = useState(null);
 
   useEffect(() => {
-    if (countries) {
-      axios
-        .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
-        .then((res) => setCountries(res.data));
-    }
+    axios
+      .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
+      .then((res) => setCountries(res.data));
   }, []);
+
+  useEffect(() => {
+    if (selectedCountry) {
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${
+            selectedCountry.capital
+          }&appid=${import.meta.env.VITE_OPENWEATHER_API}`
+        )
+        .then((res) => setSelectedCountryWeather(res.data));
+    }
+  }, [selectedCountry]);
 
   const onSearch = (e) => {
     const search = e.target.value;
     setSearch(search);
-    setSelectedCountry(null)
+    setSelectedCountry(null);
     setFilteredCountries(
       countries.filter((country) =>
         country.name.common.toLowerCase().includes(search)
@@ -33,7 +44,8 @@ const App = () => {
     if (filteredCountries.length > 1)
       return filteredCountries.map((country) => (
         <p key={country.name.common}>
-          {country.name.common} <button onClick={() => setSelectedCountry(country)}>Show</button>
+          {country.name.common}{" "}
+          <button onClick={() => setSelectedCountry(country)}>Show</button>
         </p>
       ));
     if (filteredCountries.length === 1) {
@@ -43,6 +55,7 @@ const App = () => {
   };
 
   const renderCountryInfo = () => {
+    {console.log("hello")}
     const country = selectedCountry;
     return (
       <>
@@ -56,6 +69,22 @@ const App = () => {
           })}
         </ul>
         <img src={country.flags.png} alt={`Flag of ${country.name.common}`} />
+        <h2>Weather in {country.capital}</h2>
+        {!selectedCountryWeather ? (
+          "Loading"
+        ) : (
+          <>
+            <p>
+              Temperature:{" "}
+              {(selectedCountryWeather.main.temp - 273.15).toFixed(2)} Celcius
+            </p>
+            <img
+              src={`https://openweathermap.org/img/wn/${selectedCountryWeather.weather[0].icon}@2x.png`}
+              alt=""
+            />
+            <p>Wind {selectedCountryWeather.wind.speed} m/s</p>
+          </>
+        )}
       </>
     );
   };
@@ -65,7 +94,7 @@ const App = () => {
       <form>
         find countries: <input value={search} onChange={onSearch} />
       </form>
-      <div>{selectedCountry ? renderCountryInfo() :renderCountryList()}</div>
+      <div>{selectedCountry ? renderCountryInfo() : renderCountryList()}</div>
     </div>
   );
 };
