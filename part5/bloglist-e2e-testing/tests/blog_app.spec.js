f9317512ab49e86c25223b9763966e818d5073d9
Zed <zedcaoile@gmail.com>
2025-10-04 19:32:16 +0800

    submit exer 5.17

---
+++ part5/bloglist-e2e-testing/tests/blog_app.spec.js
@@ -0,0 +1,13 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    await expect(page.getByText('Log in to application')).toBeVisible()
+    await expect(page.getByLabel('username:')).toBeVisible()
+    await expect(page.getByLabel('password:')).toBeVisible()
+  })
+})
