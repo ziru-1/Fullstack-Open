d8a8ccab1424bf47dc93b0e737777051b8b03a4d
Zed <zedcaoile@gmail.com>
2025-10-21 20:08:07 +0800

    submit exer 6.21

--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,9 +1,23 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { createAnecdote } from "../requests/anecdoteRequest"
+
+
 const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+
+  const newAnecdoteMutation = useMutation({
+    mutationFn: createAnecdote,
+    onSuccess: (createdAnecdote) => {
+      const anecdotes = queryClient.getQueryData(['anecdotes'])
+      queryClient.setQueryData(['anecdotes'], anecdotes.concat(createdAnecdote))
+    }
+  })
+
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    console.log('new anecdote')
+    newAnecdoteMutation.mutate({ content, votes: 0 })
   }
 
   return (
--- part6/query-anecdotes/src/requests/anecdoteRequest.js
+++ part6/query-anecdotes/src/requests/anecdoteRequest.js
@@ -10,4 +10,21 @@ const getAll = async () => {
   return response.json()
 }
 
-export { getAll }
+const createAnecdote = async (newAnecdote) => {
+  console.log(newAnecdote)
+  const options = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(newAnecdote)
+  }
+ 
+  const response = await fetch(baseUrl, options)
+ 
+  if (!response.ok) {
+    throw new Error('Failed to create anecdote')
+  }
+ 
+  return await response.json()
+}
+
+export { getAll, createAnecdote }
