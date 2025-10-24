70eb50981b1d22dbfe1a3194239f3b5dfa964aac
Zed <zedcaoile@gmail.com>
2025-10-24 18:35:39 +0800

    submit exer 6.24

--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,8 +1,7 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query"
-import { createAnecdote } from "../requests/anecdoteRequest"
-import { useContext } from "react"
-import NotifContext from "../NotifContext"
-
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { createAnecdote } from '../requests/anecdoteRequest'
+import { useContext } from 'react'
+import NotifContext from '../NotifContext'
 
 const AnecdoteForm = () => {
   const { notifDispatch } = useContext(NotifContext)
@@ -13,18 +12,31 @@ const AnecdoteForm = () => {
     onSuccess: (createdAnecdote) => {
       const anecdotes = queryClient.getQueryData(['anecdotes'])
       queryClient.setQueryData(['anecdotes'], anecdotes.concat(createdAnecdote))
-    }
+    },
   })
 
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
+
+    if (content.length < 5) {
+      notifDispatch({
+        type: 'SET_NOTIF',
+        payload: 'too short anecdote, must have length 5 or more',
+      })
+      setTimeout(() => {
+        notifDispatch({ type: 'CLEAR_NOTIF' })
+      }, 5000)
+
+      return
+    }
+
     event.target.anecdote.value = ''
     newAnecdoteMutation.mutate({ content, votes: 0 })
-    notifDispatch({type: 'SET_NOTIF', payload: `${content} has been added`})
+    notifDispatch({ type: 'SET_NOTIF', payload: `${content} has been added` })
     setTimeout(() => {
-      notifDispatch({type: 'CLEAR_NOTIF'})
-    }, 5000);
+      notifDispatch({ type: 'CLEAR_NOTIF' })
+    }, 5000)
   }
 
   return (
