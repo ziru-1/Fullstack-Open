39f3ff182b3edd5991c75b229c6899a48e8e92eb
Zed <zedcaoile@gmail.com>
2026-02-08 06:22:47 +0800

    submit exer 9.6

--- part9/index.ts
+++ part9/index.ts
@@ -1,33 +1,33 @@
-import express from 'express'
-import { calculateBmi } from './bmiCalculator'
+import express from 'express';
+import { calculateBmi } from './bmiCalculator';
 
-const app = express()
-const port = 3003
+const app = express();
+const port = 3003;
 
 app.get('/hello', (_req, res) => {
-  res.send('Hello Full Stack!')
-})
+  res.send('Hello Full Stack!');
+});
 
 app.get('/bmi', (req, res) => {
-  const height = Number(req.query.height)
-  const weight = Number(req.query.weight)
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
 
   if (isNaN(height) || isNaN(weight)) {
     res.status(400).json({
       error: 'malformatted parameters',
-    })
-    return
+    });
+    return;
   }
 
-  const bmi = calculateBmi(height, weight)
+  const bmi = calculateBmi(height, weight);
 
   res.json({
     height,
     weight,
     bmi,
-  })
-})
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
