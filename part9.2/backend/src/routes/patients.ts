4de715f2a49f4aedbd2a90f0c197e2a9b5b7ceee
Zed <zedcaoile@gmail.com>
2026-02-23 17:54:57 +0800

    submit exer 9.12

--- part9.2/backend/src/routes/patients.ts
+++ part9.2/backend/src/routes/patients.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Response } from 'express';
 import { PublicPatient } from '../types';
 import express from 'express';
@@ -7,6 +9,18 @@ const router = express.Router();
 
 router.get('/', (_req, res: Response<PublicPatient[]>) => {
   res.send(patientService.getPatients());
+});
+
+router.post('/', (req, res) => {
+  const { name, dateOfBirth, ssn, gender, occupation } = req.body;
+  const addedEntry = patientService.addPatient(
+    name,
+    dateOfBirth,
+    ssn,
+    gender,
+    occupation,
+  );
+  res.json(addedEntry);
 });
 
 export default router;
--- part9.2/backend/src/services/patientService.ts
+++ part9.2/backend/src/services/patientService.ts
@@ -1,5 +1,6 @@
 import patientData from '../../data/patients';
 import { Patient, PublicPatient } from '../types';
+import { v1 as uuid } from 'uuid';
 
 const patients: Patient[] = patientData;
 
@@ -8,6 +9,27 @@ const getPatients = (): PublicPatient[] => {
   return patients.map(({ ssn, ...rest }) => rest);
 };
 
+const addPatient = (
+  name: string,
+  dateOfBirth: string,
+  ssn: string,
+  gender: string,
+  occupation: string,
+): Patient => {
+  const newPatientEntry = {
+    id: uuid(),
+    name,
+    dateOfBirth,
+    ssn,
+    gender,
+    occupation,
+  };
+
+  patients.push(newPatientEntry);
+  return newPatientEntry;
+};
+
 export default {
   getPatients,
+  addPatient,
 };
