e1782615c0bbeb800062ba82ef7158b8bc4b1725
Zed <zedcaoile@gmail.com>
2026-03-11 21:19:30 +0800

    submit exer 9.18

--- part9/part9.4/frontend/src/App.tsx
+++ part9/part9.4/frontend/src/App.tsx
@@ -1,9 +1,14 @@
 import { useEffect, useState } from 'react'
-import { getAllDiaries } from './services/diaryService'
-import type { DiaryEntry } from './types'
+import { createDiary, getAllDiaries } from './services/diaryService'
+import type { DiaryEntry, Visibility, Weather } from './types'
 
 const App = () => {
   const [diaries, setDiaries] = useState<DiaryEntry[]>([])
+
+  const [date, setDate] = useState('')
+  const [visibility, setVisibility] = useState('')
+  const [weather, setWeather] = useState('')
+  const [comment, setComment] = useState('')
 
   useEffect(() => {
     const fetchDiaries = async () => {
@@ -18,8 +23,62 @@ const App = () => {
     fetchDiaries()
   }, [])
 
+  const handleSubmit = async (e: React.SyntheticEvent) => {
+    e.preventDefault()
+
+    const newEntry = await createDiary({
+      date,
+      visibility: visibility as Visibility,
+      weather: weather as Weather,
+      comment,
+    })
+
+    setDiaries(diaries.concat(newEntry))
+  }
+
   return (
     <div>
+      <h2>Add new entry</h2>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          date
+          <input
+            type='text'
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
+        </div>
+
+        <div>
+          visibility
+          <input
+            type='text'
+            value={visibility}
+            onChange={(e) => setVisibility(e.target.value)}
+          />
+        </div>
+
+        <div>
+          weather
+          <input
+            type='text'
+            value={weather}
+            onChange={(e) => setWeather(e.target.value)}
+          />
+        </div>
+
+        <div>
+          comment
+          <input
+            type='text'
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+          />
+        </div>
+
+        <button type='submit'>add</button>
+      </form>
       <h2>Diary entries</h2>
       {diaries.map((diary) => (
         <div key={diary.id}>
--- part9/part9.4/frontend/src/services/diaryService.ts
+++ part9/part9.4/frontend/src/services/diaryService.ts
@@ -1,9 +1,14 @@
 import axios from 'axios'
-import type { DiaryEntry } from '../types'
+import type { DiaryEntry, NewDiaryEntry } from '../types'
 
 const baseUrl = 'http://localhost:3000/api/diaries'
 
 export const getAllDiaries = async () => {
   const response = await axios.get<DiaryEntry[]>(baseUrl)
+  return response.data
+}
+
+export const createDiary = async (object: NewDiaryEntry) => {
+  const response = await axios.post<DiaryEntry>(baseUrl, object)
   return response.data
 }
