9f56e60950b4ec5ebce81cdd692ffd70d14a9d26
Zed <zedcaoile@gmail.com>
2026-03-26 18:47:17 +0800

    submit exer 9.26

---
+++ part9/part9.2/frontend/src/components/PatientListPage/EntryDetails.tsx
@@ -0,0 +1,97 @@
+import { Box, List, ListItem, Typography } from '@mui/material';
+import LocalHospitalIcon from '@mui/icons-material/LocalHospital';
+import MedicalServicesIcon from '@mui/icons-material/MedicalServices';
+import WorkIcon from '@mui/icons-material/Work';
+import { Entry } from '../../types';
+import { assertNever } from '../../utils';
+
+interface Props {
+  entry: Entry;
+  getDiagnosisName: (code: string) => string;
+}
+
+const EntryDetails = ({ entry, getDiagnosisName }: Props) => {
+  const baseStyle = {
+    border: '2px solid black',
+    borderRadius: '5px',
+    padding: '10px',
+    marginTop: '10px',
+  };
+
+  const getHealtCheckRatingIcon = (rating: number): string => {
+    switch (rating) {
+      case 0:
+        return '💚';
+      case 1:
+        return '💛';
+      default:
+        return '❓';
+    }
+  };
+
+  switch (entry.type) {
+    case 'Hospital':
+      return (
+        <Box style={baseStyle} sx={{ mb: 2 }}>
+          <Typography>
+            {entry.date} <LocalHospitalIcon />
+          </Typography>
+          <Typography>
+            <em>{entry.description}</em>
+          </Typography>
+
+          {entry.diagnosisCodes && entry.diagnosisCodes.length > 0 && (
+            <List sx={{ listStyleType: 'disc', pl: 4 }}>
+              {entry.diagnosisCodes.map((code) => (
+                <ListItem key={code} sx={{ display: 'list-item', py: 0 }}>
+                  <Typography variant='body2'>
+                    {code} {getDiagnosisName(code)}
+                  </Typography>
+                </ListItem>
+              ))}
+            </List>
+          )}
+
+          {entry.discharge && (
+            <Typography>
+              <strong>Discharge: </strong>
+              {entry.discharge.date} - {entry.discharge.criteria}
+            </Typography>
+          )}
+
+          <Typography>diagnose by: {entry.specialist}</Typography>
+        </Box>
+      );
+    case 'HealthCheck':
+      return (
+        <Box style={baseStyle} sx={{ mb: 2 }}>
+          <Typography>
+            {entry.date} <MedicalServicesIcon />
+          </Typography>
+          <Typography>
+            <em>{entry.description}</em>
+          </Typography>
+          <Typography>
+            {getHealtCheckRatingIcon(entry.healthCheckRating)}
+          </Typography>
+          <Typography>diagnose by: {entry.specialist}</Typography>
+        </Box>
+      );
+    case 'OccupationalHealthcare':
+      return (
+        <Box style={baseStyle} sx={{ mb: 2 }}>
+          <Typography>
+            {entry.date} <WorkIcon /> {entry.employerName}
+          </Typography>
+          <Typography>
+            <em>{entry.description}</em>
+          </Typography>
+          <Typography>diagnose by: {entry.specialist}</Typography>
+        </Box>
+      );
+    default:
+      return assertNever(entry);
+  }
+};
+
+export default EntryDetails;
